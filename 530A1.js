cf5fa11f6e6e5b92953bc9b6ec4b6e42c76e1a46
Marc Zych <marczych@gmail.com>
2012-04-09 12:17:35 -0700

    Output variable names on the page

--- 530A1.js
+++ 530A1.js
@@ -1,7 +1,31 @@
 window.addEvent('domready', function() {
-   var programInput = $('programInput');
-
-   $('submitButton').addEvent('click', function() {
-      alert(programInput.value);
-   }.bind(this));
+   VariableFinder.initialize();
 });
+
+var VariableFinder = {
+   initialize: function() {
+      var programInput = $('programInput');
+      var output = $('output');
+
+      $('submitButton').addEvent('click', function() {
+         var ast = parse(programInput.value);
+         var variables = this.getVariables(ast);
+
+         output.getChildren().each(function(element) {
+            element.dispose();
+         });
+
+         variables.each(function(varName) {
+            var row = new Element('p', {
+               html: varName
+            });
+
+            row.inject(output);
+         }.bind(this));
+      }.bind(this));
+   },
+
+   getVariables: function(ast) {
+      return ['var1', 'var2', 'var3'];
+   }
+}
