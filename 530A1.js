f0412fb8237890ff92946d9c8b73a82d29642737
Marc Zych <marczych@gmail.com>
2012-04-11 01:24:09 -0700

    Final prep for submission

--- 530A1.js
+++ 530A1.js
@@ -8,8 +8,7 @@ var VariableFinder = {
       var output = $('output');
 
       $('submitButton').addEvent('click', function() {
-         var ast = parse(programInput.value);
-         var variables = this.getVariables(ast);
+         var variables = this.getVariables(programInput.value);
 
          output.getChildren().each(function(element) {
             element.dispose();
@@ -25,7 +24,43 @@ var VariableFinder = {
       }.bind(this));
    },
 
-   getVariables: function(ast) {
-      return ['var1', 'var2', 'var3'];
+   getVariables: function(input) {
+      var variables = [];
+
+      try {
+         var ast = parse(input);
+         this.getVariablesHelper(ast, variables);
+         variables.sort();
+      } catch (exc) {
+         this.addVariable(variables,
+          'Sorry there was a parse error with your javascript: ' +
+          '<pre>' + exc + '</pre>');
+      }
+
+      return variables;
+   },
+
+   getVariablesHelper: function(ast, variables) {
+      ast.each(function(node) {
+         if (node == null) {
+            return;
+         }
+
+         if (typeof node == 'object') {
+            if (node[0] == 'name') {
+               this.addVariable(variables, node[1]);
+            } else {
+               this.getVariablesHelper(node, variables);
+            }
+         }
+      }.bind(this));
+
+      return variables;
+   },
+
+   addVariable: function(variables, varName) {
+      if (variables.indexOf(varName) == -1) {
+         variables.push(varName);
+      }
    }
 }
